48f65b1272f8f6b2e21542b1b2ab60ab11a836e0
Glenn Schmottlach <Glenn.Schmottlach@xs-embeddedd.com>
2013-04-20 19:49:53 -0400

    Improvements to cmake target build.

--- build_target.sh
+++ build_target.sh
@@ -17,11 +17,39 @@ do
     fi    
 done
 
-# Suck in the environment settings for pkg-conf
-. /usr/local/sdk-imx6/environment-setup
+# Set some defaults
+if [ -d /usr/local/sdk-imx6 ]; then
+    CMAKE_TOOLCHAIN_PATH_PREFIX="/usr/local/sdk-imx6"
+else
+    CMAKE_TOOLCHAIN_PATH_PREFIX="$PWD"
+fi
 
-BUILD_TARGET=armv7l
+
+
+# Set up the environment for the build tools
+if [ -e "${CMAKE_TOOLCHAIN_PATH_PREFIX}/environment-setup" ]; then
+    . "${CMAKE_TOOLCHAIN_PATH_PREFIX}/environment-setup"
+else
+    echo -e "Target environment may not be set up properly!\n"
+fi
+
+if [ ! -e "${CMAKE_TOOLCHAIN_PATH_PREFIX}/cmake.toolchain" ]; then
+   echo "cmake.toolchain file is not found!"
+   exit 1 
+fi
+
+# Determine the build target
+BUILD_TARGET=$(cat "${CMAKE_TOOLCHAIN_PATH_PREFIX}/cmake.toolchain" | \
+                perl -n -e'/CMAKE_SYSTEM_PROCESSOR\s+(\w+)\)/ && print $1')
+if [ "${BUILD_TARGET}" = "" ]; then
+    BUILD_TARGET=armv7l
+    echo "Build target not found - assuming default (${BUILD_TARGET})"
+fi
+
 BUILD_DIR="${BUILD_TARGET}-${BUILD_TYPE}"
+
+echo -e "==> Running cmake for ${BUILD_DIR} . . .\n"
+
 
 if [ ! -d ${BUILD_DIR} ]; then
   mkdir ${BUILD_DIR}
@@ -35,7 +63,7 @@ if [ ! -e Makefile ]; then
   cmake -DCMAKE_SYSTEM_PROCESSOR=${BUILD_TARGET} \
         -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
         -DCMAKE_INSTALL_PREFIX="${BUILD_DIR_PATH}/install" \
-        -DCMAKE_TOOLCHAIN_FILE=/usr/local/sdk-imx6/cmake.toolchain \
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_PATH_PREFIX}/cmake.toolchain \
         "${args[@]}" ..
   if [ $? != 0 ]; then
     exit $_
