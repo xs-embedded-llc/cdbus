f7bf253e8061a5e07877263f533bc4fc3ecab436
Tysen Moore <tysen.moore@xs-embedded.com>
2013-05-20 14:52:10 -0400

    - Created a simple ping stress tool for cdbus.  Compatible with the l2dbus ping stress tool - Could still:   - Add the bus selection (using --bus)   - Implement the echo API for Dbus, not just the ldbus compatible "svc IPC" like interface   - Fix -k option   - Shutdown is still a problem

---
+++ test/ping_stress2/build_host.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+BUILD_TYPE="Release"
+i=0
+args=()
+for arg in "$@"
+do
+    if [ "$arg" = "debug" ]
+    then
+        BUILD_TYPE="Debug"
+    elif [ "$arg" = "release" ]
+    then
+        BUILD_TYPE="Release"
+    elif [ "${arg:0:19}" = "-DCMAKE_BUILD_TYPE=" ]
+    then
+        BUILD_TYPE="${arg:19}"
+    else
+        args[$i]="$arg"
+        ((++i))
+    fi    
+done
+
+BUILD_TARGET=`uname -p`
+BUILD_DIR="${BUILD_TARGET}-${BUILD_TYPE}"
+
+if [ ! -d ${BUILD_DIR} ]; then
+  mkdir ${BUILD_DIR}
+fi
+
+cd ${BUILD_DIR}
+
+# To change the installation prefix pass in the following CMAKE definition:
+# -DCMAKE_INSTALL_PREFIX=~/workspace/l2dbus/x86_64-Release/install
+
+if [ ! -e Makefile ]; then
+  cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE "${args[@]}" ..
+  if [ $? != 0 ]; then
+    exit $_
+  fi
+fi
+
+make
+
+exit $?
